58f893273e18feee68d9c1f909287c507a3e1e5a
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2024-03-04 10:52:37 -0300

    Update and rename a.md to 2023-12-09-modelo-geológico-taubaté.m

---
+++ _articles/2023-12-09-modelo-geológico-taubaté.m
@@ -0,0 +1,27 @@
+---
+title: "Modelo Geológico 3D da Bacia de Taubaté e aplicação na
+pesquisa educacional em Geociências "
+category: articles
+permalink: /articles/2023-12-09-modelo-geológico-taubaté/
+venue: "Terrae Didatica"
+excerpt: ''
+date: 2023-12-09
+venue: 'Terrae Didatica'
+citation: 'Conclusão do Modelo Geológico
+3D da Bacia de Taubaté e aplicação na pesquisa
+educacional em Geociências. Terræ Didatica,
+19(Publ. Contínua), 1-8, e023038. doi: 10.20396/
+td.v19i00.8675136.'
+---
+
+<a href='_articles/Modelo_geologico_3D_da_bacia_de_Taubate_e_aplicaca.pdf'>Download o PDF aqui</a>
+
+Abstract: Introduction and Objective. This article summarizes the results of a research project that aimed to produce three-dimensional models of the Taubaté Sedimentary Basin. The project is part of a larger body of research, which aims to generate 3D representations and models of sedimentary basins and relief features in Brazil for didactic purposes. Methodology. The research method involved bibliographic review, systematic study of articles and 3D computer modeling for the production of physical models. The work involved processing data from spectral remote sensors and data from deep drillings carried out in the basin. Results. Each generated model will compose a collection of Geoscience teaching materials for basic schools and universities. The kits and materials should be distributed to primary and higher education teachers, through workshops and tutorials on the didactic use of the resources. The three-dimensional model of the Taubaté Basin will contribute to a better understanding of the morphology, structure and geological history of this important basin of São Paulo State. Conclusion. Teaching guides that will help basic education teachers to use models in their teaching activities are also under development..
+
+ Recommended citation: Carneiro, C. D. R., Carvalho, R.
+S. de, Padovani, S. G., Ferreira, R., Pinto, A. V. F., &
+Vieira, L. C. (2023). Conclusão do Modelo Geológico
+3D da Bacia de Taubaté e aplicação na pesquisa
+educacional em Geociências. Terræ Didatica,
+19(Publ. Contínua), 1-8, e023038. doi: 10.20396/
+td.v19i00.8675136.
